eb22c6faac6bba09252738bc24aa2b1d9981c701
Leydydiaz <lmdiazm1@gmail.com>
2025-04-14 23:06:34 -0500

    Update data.py

---
+++ data.py
@@ -0,0 +1,2 @@
+URL_SERVICE = "https://cnt-d24eac88-6d9a-4097-8496-0697d34961ed.containerhub.tripleten-services.com/api/v1"
+URL_SERVICE = "https://cnt-d24eac88-6d9a-4097-8496-0697d34961ed.containerhub.tripleten-services.com/api/v1/kits"
