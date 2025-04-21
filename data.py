0a46fac1f9943bd63da1641ffb16daaa57aeef91
Leydy Diaz <lmdiazm1@gmail.com>
2025-04-21 16:41:50 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,2 +1,16 @@
-URL_SERVICE = "https://cnt-d24eac88-6d9a-4097-8496-0697d34961ed.containerhub.tripleten-services.com/api/v1"
-URL_SERVICE = "https://cnt-d24eac88-6d9a-4097-8496-0697d34961ed.containerhub.tripleten-services.com/api/v1/kits"
+URL_SERVICE = " https://cnt-e8390e6e-e63a-46a5-a1fc-de79e27747ef.containerhub.tripleten-services.com/api/v1/users"
+
+{
+  "firstName": "Max",
+  "phone": "+10005553535",
+  "address": "8042 Lancaster Ave.Hamburg, NY"
+} 
+
+URL_SERVICE = " https://cnt-4aeed52c-a504-4818-9fbc-9a12aca12949.containerhub.tripleten-services.com/api/v1/kits"
+
+{
+    "name": "Mi kit",
+    "productsList": null,
+    "id": 7,
+    "productsCount": 0
+}
