5dd743923b228a8df77a2f0ff822d842ba709222
Leydy Diaz <lmdiazm1@gmail.com>
2025-04-21 16:51:59 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,22 +1,25 @@
-URL_SERVICE = " https://cnt-e8390e6e-e63a-46a5-a1fc-de79e27747ef.containerhub.tripleten-services.com/api/v1/users"
-
-{
-  "firstName": "Max",
-  "phone": "+10005553535",
-  "address": "8042 Lancaster Ave.Hamburg, NY"
-} 
-
-(201 Created) 
-{
-    "authToken": "da2cd123-50f3-4a12-bcbd-380010063dc0"
+headers = {
+    "Content-Type": "application/json"
 }
 
+user_body = {
+    "firstName": "Max",
+    "phone": "+10005553535",
+    "address": "8042 Lancaster Ave.Hamburg, NY"
+}
 
-URL_SERVICE = " https://cnt-4aeed52c-a504-4818-9fbc-9a12aca12949.containerhub.tripleten-services.com/api/v1/kits"
-
-{
+kit_body = {
     "name": "Mi kit",
+    "user": {
+        "id": 1,
+        "firstName": "Max",
+        "phone": "+10005553535",
+        "address": "8042 Lancaster Ave.Hamburg, NY",
+        "email": "",
+        "comment": "",
+        "authToken": "da2cd123-50f3-4a12-bcbd-380010063dc0"
+    },
     "productsList": null,
     "id": 7,
-    "productsCount": 0
+    "productsCount": 0
 }
