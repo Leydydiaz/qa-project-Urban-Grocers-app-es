767e8fc06bdcc886621cc121b6d2ec88bbdaa8a1
Leydy Diaz <lmdiazm1@gmail.com>
2025-04-21 16:43:45 -0500

    Update data.py

--- data.py
+++ data.py
@@ -6,6 +6,12 @@ URL_SERVICE = " https://cnt-e8390e6e-e63a-46a5-a1fc-de79e27747ef.containerhub.tr
   "address": "8042 Lancaster Ave.Hamburg, NY"
 } 
 
+(201 Created) 
+{
+    "authToken": "da2cd123-50f3-4a12-bcbd-380010063dc0"
+}
+
+
 URL_SERVICE = " https://cnt-4aeed52c-a504-4818-9fbc-9a12aca12949.containerhub.tripleten-services.com/api/v1/kits"
 
 {
