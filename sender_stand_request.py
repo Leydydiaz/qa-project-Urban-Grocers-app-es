ba188cde65c8937e5bf3e79030300a88e195f3d7
Leydy Diaz <lmdiazm1@gmail.com>
2025-04-28 16:17:41 -0500

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,17 +1,28 @@
-def post_new_user(body):
-    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
-                         json=body,
-                         headers=configuration.headers)
+import requests
+import data
+import configuration
 
+# Crear un nuevo usuario
+def post_new_user(body):
+    return requests.post(
+        configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+        json=body,
+        headers=configuration.headers
+    )
+
+# Crear el cuerpo (body) de un kit, usando solo el nombre
 def get_kit_body(name):
     kit_body = {
         "name": name
     }
     return kit_body
 
+# Crear un nuevo kit
 def post_new_kit(kit_body, auth_token):
-    headers = configuration.headers.copy()
+    headers = configuration.headers.copy()  # Copiamos para no modificar el original
     headers["Authorization"] = f"Bearer {auth_token}"
-    return requests.post(configuration.URL_SERVICE + configuration.KITS_PATH,
-                         json=kit_body,
-                         headers=headers)
+    return requests.post(
+        configuration.URL_SERVICE + configuration.KITS_PATH,
+        json=kit_body,
+        headers=headers
+    )
