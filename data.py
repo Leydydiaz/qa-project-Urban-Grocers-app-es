0f31253dfaee1927abe7edebd4b2ce2379ea31e4
Leydy Diaz <lmdiazm1@gmail.com>
2025-04-28 16:10:58 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,25 +1,26 @@
+# data.py
+
+# Información básica de un usuario
+user_body = {
+    "firstName": "Max",
+    "phone": "+10005553535",
+    "address": "8042 Lancaster Ave. Hamburg, NY"
+}
+
+# Cabeceras (headers) para la solicitud HTTP
 headers = {
     "Content-Type": "application/json"
 }
 
-user_body = {
-    "firstName": "Max",
-    "phone": "+10005553535",
-    "address": "8042 Lancaster Ave.Hamburg, NY"
-}
+# Variables para pruebas POSITIVAS
+one_letter = "a"
+max_characters = "A" * 511  # 511 caracteres
+special_characters = "№%@,"
+spaces_in_name = " A Aaa "
+numbers_in_name = "123"
 
-kit_body = {
-    "name": "Mi kit",
-    "user": {
-        "id": 1,
-        "firstName": "Max",
-        "phone": "+10005553535",
-        "address": "8042 Lancaster Ave.Hamburg, NY",
-        "email": "",
-        "comment": "",
-        "authToken": "da2cd123-50f3-4a12-bcbd-380010063dc0"
-    },
-    "productsList": null,
-    "id": 7,
-    "productsCount": 0
-}
+# Variables para pruebas NEGATIVAS
+empty_name = ""
+exceeds_max_characters = "A" * 512  # 512 caracteres
+missing_name_field = {}  # kit_body sin el campo name
+different_type_name = 123  # name no como string, sino número
